e631d730d61ce476d263bf6343c97d79648d6ffd
Muhammed Muslih <sendmailtomuslih@gmail.com>
2024-04-29 20:26:57 +0530

    unwanted data removed

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,50 +20,6 @@ const getAllUsers = asyncHandler(async (req, res, next) => {
   });
 });
 
-//post readExcel
-//@route POST api/admin/read-excel
-const readExcel = asyncHandler(async (req, res, next) => {
-  // Reading the Excel file
-  const allTables = [];
-  const workbook = xlsx.readFile("./public/race1.xlsx");
-  workbook.SheetNames.forEach((sheetName) => {
-    const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-    let currentTableStart = null;
-    let currentTableData = [];
 
-    sheetData.forEach((row, rowIndex) => {
-      const isEmptyRow = (row) =>
-        Object.values(row).every((value) => value === undefined || value === "");
 
-      if (isEmptyRow(row)) {
-        // Empty row encountered
-        if (currentTableStart !== null) {
-          // Table end detected, add previous table data
-          allTables.push({ data: [sheetData.slice(currentTableStart, rowIndex)] });
-          currentTableStart = null;
-          currentTableData = [];
-        }
-      } else {
-        // Non-empty row encountered, start or continue table data
-        if (currentTableStart === null) {
-          currentTableStart = rowIndex;
-        }
-        currentTableData.push(row);
-      }
-    });
-
-    // Handle the last table if it doesn't end with an empty row
-    if (currentTableStart !== null) {
-      allTables.push({ data: sheetData.slice(currentTableStart) });
-    }
-  });
-
-  console.log(allTables);
-
-  res.status(200).send({
-    status: "success",
-    message: allTables,
-  });
-});
-
-export { getAllUsers, readExcel };
+export { getAllUsers };
--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,5 @@ import adminAuth from "../middlewares/adminAuth.js";
 const router = express.Router();
 
 router.get("/get-all-users", adminAuth, adminController.getAllUsers);
-router.post("/read-excel", adminController.readExcel);
 
 export default router;
