a123bfece61748e10a2bd34b7dafe75602d5568e
Muhammed Muslih <sendmailtomuslih@gmail.com>
2024-05-07 12:42:53 +0530

    race finding method updated

--- services/race.js
+++ services/race.js
@@ -3,8 +3,8 @@ import Race from "../models/raceModel.js";
 const addNewRace = async (horses, date) => await Race.create({ date: date, horses });
 
 const findRacesByDate = async (date) => {
-  const formattedDate = new Date(date);
 
+  const formattedDate = new Date(date);
   const races = await Race.find({
     date: {
       $gte: formattedDate.setHours(0, 0, 0, 0),
