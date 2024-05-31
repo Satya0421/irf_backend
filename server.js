4aaf8fa45565013ef641d74abfd8c68711415b3d
Muhammed Muslih <sendmailtomuslih@gmail.com>
2024-05-31 13:35:11 +0530

    socket.io server configration changed

--- server.js
+++ server.js
@@ -18,14 +18,7 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-
-const io = new Server(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: ["http://localhost:5173", "https://irfadminpanel.onrender.com"],
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server);
 
 //socket connection configration
 socketConfig(io);
